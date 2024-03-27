78ec0bd48e2a95075785952e8cb2a7066a3b5931
sadia-tahsin <sadia-tahsin3.1415@gmail.com>
2024-03-27 19:35:50 +0600

    hosting done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({origin:"http://localhost:5173", credentials:true}));
+app.use(cors({origin:"https://imposing-ratio-376319.web.app", credentials:true}));
 //app.use(cors())
 app.use(express.json());
 
